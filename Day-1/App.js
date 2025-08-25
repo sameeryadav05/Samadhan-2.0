85fa0115abaae5cea7b6fe204b3f57fcffc0cee9
Sameer yadav <sameer.manoj2005@gmail.com>
2025-08-25 22:59:38 +0530

    Day 1 task print student details from an object

---
+++ Day-1/App.js
@@ -0,0 +1,13 @@
+// student.js
+const student = {
+  name: "Sameer Yadav",
+  age: 21,
+  course: "B.Tech CSE",
+  marks: 89
+};
+
+console.log("Student Details:");
+console.log(`Name: ${student.name}`);
+console.log(`Age: ${student.age}`);
+console.log(`Course: ${student.course}`);
+console.log(`Marks: ${student.marks}`);
